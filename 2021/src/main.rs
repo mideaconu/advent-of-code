612e933aa88d9cf45b711f7236321da99fbefec8
mihai.deaconu <mihai.deaconu@benevolent.ai>
2021-12-01 17:05:04 +0200

    feat: add prototype CLI

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,3 +1,17 @@
+mod challenges;
+
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    // Day
+    day: u8,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    input_file: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
 }
