8d0efd4906df085b5480e8ba724aaaa8d2804d62
mihai.deaconu <mihai.deaconu@benevolent.ai>
2021-12-02 15:14:56 +0200

    feat: solution part 1

---
+++ 2021/src/challenges/day2.rs
@@ -0,0 +1,43 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+
+struct Path {
+    horizontal: u32,
+    depth: u32,
+}
+
+impl Path {
+    fn area(&self) -> u32 {
+        self.horizontal * self.depth
+    }
+}
+
+
+fn solve_problem_1(input: &Vec<Vec<String>>) -> u32 {
+    let mut path = Path { horizontal: 0, depth: 0 };
+    for command in input {
+        let step = command[1].parse::<u32>().unwrap();
+        match command[0].as_str() {
+            "forward" => path.horizontal += step,
+            "up" => path.depth -= step,
+            "down" => path.depth += step,
+            _ => ()
+        }
+    }
+    return path.area();
+}
+
+
+pub fn solve(input_path: std::path::PathBuf) {
+    let file = File::open(input_path).expect("file wasn't found.");
+    let reader = BufReader::new(file);
+
+    let commands: Vec<Vec<String>> = reader
+        .lines()
+        .map(|line| line.unwrap().split_whitespace().map(String::from).collect::<Vec<String>>())
+        .collect();
+
+    println!("Solution to part 1: {}", solve_problem_1(&commands));
+}
